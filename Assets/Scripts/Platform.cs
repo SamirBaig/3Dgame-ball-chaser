953e31cb75fd29a1730ca3ceaa6054711009e052
Samir <samirbaig79@gmail.com>
2022-10-29 15:51:58 +0500

    first commit

---
+++ Assets/Scripts/Platform.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Platform : MonoBehaviour {
+
+	void OnTriggerEnter(Collider collider) { 
+		if (collider.gameObject.tag == "Player") { //player = ball script
+			Invoke ("FallDown", 0.6f); // 0.6 seconds wait till platform falls
+		}
+	}
+	//this will fall platform
+	private void FallDown() {
+		this.GetComponentInParent<Rigidbody>().isKinematic = false; 
+		Destroy (this.transform.parent.gameObject, 2f); //0.6 seconds to fall and 2 seconds to destroy fallen platform
+	}
+}
---
+++ Assets/Scripts/Rotator.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour {
+	// it is used to rotate gems
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate (new Vector3(15,20,45) * Time.deltaTime); //to rotate the gems on same speed for all devices
+	}
+}
